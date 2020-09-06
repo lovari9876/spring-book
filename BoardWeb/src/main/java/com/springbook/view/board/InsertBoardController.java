0450a1c95a9ff3421b37b1b5f8b1f9d661c9aa37
lovari <lovari9876@gmail.com>
2020-09-06 14:13:23 +0900

    358p Annotation @Controller 이용한 InsertBoardController 클래스

--- BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,17 +1,24 @@
 package com.springbook.view.board;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
-public class InsertBoardController implements Controller {
+@Controller
+public class InsertBoardController {
+	// implements Controller 삭제; 이제 Annotation 쓴다.
+	// handleRequest()를 Overriding할 필요도, response 객체를 파라미터로 받을 필요도 없어졌다.
+	// return type ModelAndView를 계속 쓸 필요도 없어졌다!
+	// Cotroller 객체임을 상속을 통해 구현하는게 아니라 POJO 스타일로 구현하게 된 것;
+	// @Controller 어노테이션 통해 스프링컨테이너가 자동으로 객체 생성 및, Controller 객체임을 인식한다.
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("/insertBoard.do") // auto HandlerMapping
+	public void insertBoard(HttpServletRequest request/* , HttpServletResponse response */) {
 
 		System.out.println("글 등록 처리");
 
@@ -33,7 +40,6 @@ public class InsertBoardController implements Controller {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("redirect:getBoardList.do");
 
-		return mav;
 	}
 
 }
